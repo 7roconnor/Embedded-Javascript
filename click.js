e164b3b97d3ce54d1b2376978cd83971c587379a
Rory O'Connor <7roconnor@stpatrick.org>
2017-03-22 14:05:53 -0500

    Add files via upload

--- click.js
+++ click.js
@@ -2,12 +2,14 @@
 function samecolor()
 {
     const doc = document;
-    let the_div = doc.querySelectorAll(#samecolor > div); //array
+    let the_div = doc.querySelectorAll("#samecolor > div"); //array
     for(let i = 0; i <the_div.length; i++)
     {
-        the_div.style.color = '#f00';
+        let opacity = (0.25 * i) + 0.25;
+        the_div[i].style.backgroundColor = '#f00';
+        the_div[i].style.opacity = opacity;
+        console.log(opacity);
     }
 }
-
 
 //declare diffcolor() here
