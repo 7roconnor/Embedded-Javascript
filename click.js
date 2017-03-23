8491c689cfa4d9c38a67136c354ab4d8487a00ba
Rory O'Connor <7roconnor@stpatrick.org>
2017-03-23 13:55:02 -0500

    Add files via upload

--- click.js
+++ click.js
@@ -8,8 +8,25 @@ function samecolor()
         let opacity = (0.25 * i) + 0.25;
         the_div[i].style.backgroundColor = '#f00';
         the_div[i].style.opacity = opacity;
-        console.log(opacity);
     }
 }
 
 //declare diffcolor() here
+function diffcolor()
+{
+    const doc = document;
+    let divs = doc.querySelectorAll('#diffcolor > div'); //array
+
+    for(let i = 0; i < divs.length; i++)
+    {
+        if(i % 2 == 0)
+        {
+            divs[i].style.backgroundColor = '#ff0';
+        }
+        else
+        {
+            divs[i].style.backgroundColor = '#00f'; 
+        }
+    }
+
+}
